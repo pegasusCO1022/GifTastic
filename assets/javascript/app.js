be9ddc3c7ff096ddab0ed0c015cb5bdac426099e
Adam Webb <spiderwebb2290@gmail.com>
2018-03-20 01:49:20 -0600

    Last Commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,13 @@
 var topics = ["bunny", "elephant", "orca", "elk", "grizzly bear", "gazelle", "cougar", "wolf"];
+var animatedGif;
+var staticGif;
 
+function renderButton(){
+    $("#topicButton").empty();
 for (var i = 0; i < topics.length; i++) {
-    var buttons = $('<button id="button">'+ topics[i] +'</button>')
+    var buttons = $('<button class="button">'+ topics[i] +'</button>')
     buttons.appendTo('#topicButton');
+}
 }
 
 $("#topicButton").on("click",'button', function(){
@@ -28,11 +33,12 @@ $.ajax({
             var gifDiv = $("<div class='col-md-4'>");
 
             var rating = results[i].rating;
-            var animatedGif = results[i].images.fixed_height.url;
-            var staticGif = results[i].images.fixed_height_still.url;
+            animatedGif = results[i].images.fixed_height.url;
+            staticGif = results[i].images.fixed_height_still.url;
             var animalImage = $('<img>');
             var p = $('<p>').text("Rating: " + rating);
 
+            animalImage.addClass("animal-div");
             animalImage.attr("src", staticGif);
             animalImage.attr("data-state", "still");
             animalImage.attr("data-still", staticGif);
@@ -49,35 +55,46 @@ $.ajax({
 })
 
 // Create a click function that animates gif images ??
-$(document).on("click", function imageChangeState() {          
+function changeSource() {          
 
     var state = $(this).attr("data-state");
     var animateImage = $(this).attr("data-animate");
     var stillImage = $(this).attr("data-still");
 
     if(state == "still") {
-        $(this).attr("src", animateGif);
+        $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
     }
 
     else if(state == "animate") {
-        $(this).attr("src", staticGif);
+        $(this).attr("src", $(this).attr('data-still'));
         $(this).attr("data-state", "still");
     }
-})
+}
 
 //When user clicks on submit or hits enter pushes what user typed into the array
 var input = document.getElementById("animalInput");
 
 // Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(e) {
-  // Cancel the default action, if needed
-  e.preventDefault();
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Trigger the button element with a click
-    document.getElementById("userInput").click();
-  }
-  topics.push(input);
+// input.addEventListener("keyup", function(e) {
+//   // Cancel the default action, if needed
+//   e.preventDefault();
+//   // Number 13 is the "Enter" key on the keyboard
+//   if (event.keyCode === 13) {
+//     // Trigger the button element with a click
+//     document.getElementById("userInput").click();
+//   }
   
-});
+// });
+
+$("#userInput").on("click", function(){
+    event.preventDefault();
+    var userSubmit = $("#animalInput").val().trim();
+    console.log(userSubmit);
+    topics.push(userSubmit);
+    renderButton();
+    $("#animalInput").val("");
+})
+
+$(document).on("click", ".animal-div", changeSource)
+renderButton();
